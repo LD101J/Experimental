522bbea857bdf9b1a9a685f99d0402863f036e88
LD101 <jmuendongei@gmail.com>
2023-05-17 21:02:45 +0300

    !st Debug

---
+++ Assets/Scripts/Print.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Print : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("i have no doors or windows"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
